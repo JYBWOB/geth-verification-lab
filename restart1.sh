38748e926492c84f59b27238e8d360258c62f687
JYBWOB <215370196@qq.com>
2022-11-18 17:23:30 +0800

    add miner api

--- restart1.sh
+++ restart1.sh
@@ -7,6 +7,6 @@ $GETH --datadir data1/ --networkid 15 --unlock 0 \
     --syncmode full \
     --password password.txt --nodiscover --rpc.evmtimeout 0 \
     --rpc.txfeecap 0 --rpc.gascap 0 --http --http.addr "0.0.0.0" \
-    --http.port 30306 --http.vhosts "*" --http.api "admin,web3,eth,debug,personal,net" \
+    --http.port 30306 --http.vhosts "*" --http.api "admin,web3,eth,debug,personal,net,miner" \
     --http.corsdomain "*" --snapshot=false \
     --allow-insecure-unlock console 2> node1.log
--- restart2.sh
+++ restart2.sh
@@ -8,6 +8,6 @@ $GETH --datadir data2/ --networkid 15 \
     --nodiscover --rpc.evmtimeout 0 \
     --rpc.txfeecap 0 --rpc.gascap 0 --http --http.addr "0.0.0.0" \
     --http.port 30307 --port 30304 --http.vhosts "*" \
-    --http.api "admin,web3,eth,debug,personal,net" \
+    --http.api "admin,web3,eth,debug,personal,net,miner" \
     --http.corsdomain "*" --snapshot=false \
     --allow-insecure-unlock console 2> node2.log
